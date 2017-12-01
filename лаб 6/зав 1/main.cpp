ec625182744c7e1b1f3160f4e9922e3c457f70df
VITALIK S <vitalik0910@gmail.com>
2017-12-01 12:31:23 +0200

    other update branche1

--- лаб 6/зав 1/main.cpp
+++ лаб 6/зав 1/main.cpp
@@ -1,46 +1,48 @@
 #include <iostream>
 #include <stdio.h>
-
-
 #include <string.h>
-#define n 3
+#include <fstream>
 using namespace std;
-struct ITNR {
-		char BEG [100] ;
-		char END [100] ;
+typedef struct ITNR {
+		string BIG ;
+		string END;
 		int NUM;
 		double DISTANSE;
-	} elemant;
+	} IT;
+	
 
 
-void input (FILE *f ,ITNR elemant[n]) {
+
+void input (FILE*f, ITNR IT) {
 	
   if ((f = fopen ("file.bin", "ab+")) == NULL)
   {
   	cout << "File not open" << endl;
   	return;
   }
+  char out;
 	cout<<"Введіть дані в масив RT "<<endl;	
 		int i=0;
-	for(int i=0;i<n;i++){
+	while (out!='e'){
 		cout<<"Введіть назву початкового пункту маршруту "<<endl;
-     	cin>>elemant[i].BEG; 
+     	cin>>IT.BIG; 
     
 	cout<<"Введіть назву кінцевого пункту маршруту "<<endl;
-     	cin>>elemant[i].END;
+     	cin>>IT.END;
     
 	cout<<"Введіть номер маршруту "<<endl;
-     	cin>>elemant[i].NUM;
+     	cin>>IT.NUM;
     
 	cout<<" Введіть відстань"<<endl;
-    	 cin>>elemant[i].DISTANSE;
-         fwrite(&elemant,sizeof(elemant),1,f);
+    	 cin>>IT.DISTANSE;
+         fwrite(&IT,sizeof(ITNR),1,f);
+         cout<<"Якщо хочете звершити ввід натисніть e:";
+         cin>>out;
 }
-
 	fclose (f);
 	}
 
-void output (FILE *f ,ITNR elemant[n]) {
+void output (FILE *f ,ITNR IT) {
 	if ((f = fopen("file.bin", "rb")) == NULL)
 {
 	cout << "Error of file. Please check and try again" << endl;
@@ -49,24 +51,23 @@ void output (FILE *f ,ITNR elemant[n]) {
 int c=0;	
 	do
 {
-	fread(&elemant[c], sizeof(elemant),1,f);
+	fread(&IT, sizeof(ITNR),1,f);
 	if (feof(f)) break;
 	c++;
 		
-	cout<<"Початок маршруту"<<elemant[i].BEG; 
-	}
-while (1);
+ 
+	}while(1);
 cout<<endl;
 char mr[100];
 			cout<<"ENTER THE NAME OF STATION - "<<endl;
 			cin>>mr;
-			for(int i=0;i<n;i++){
-			if((strcmp(elemant[i].BEG,mr)==0) || (strcmp(elemant[i].END,mr)==0))
-			cout<<"NAME OF BEGIN- "<<elemant[i].BEG<<endl;
-			cout<<"NAME OF END- "<<elemant[i].END<<endl;
-			cout<<"NUMBER- "<<elemant[i].NUM<<endl;
-			cout<<"DISTANSE- "<<elemant[i].DISTANSE<<endl;
-
+			for(int i=0;i<c;i++){
+			if( IT.BIG==mr || IT.END==mr){
+			cout<<"NAME OF BEGIN- "<<IT.BIG<<endl;
+			cout<<"NAME OF END- "<<IT.END<<endl;
+			cout<<"NUMBER- "<<IT.NUM<<endl;
+			cout<<"DISTANSE- "<<IT.DISTANSE<<endl;
+			}
 			}
 			
 		}
@@ -80,20 +81,25 @@ void clean ()
 
 int main() {
 	setlocale(LC_CTYPE,"ukr");
-	ITNR elemant[n];
+	ITNR IT;
 		FILE *f;
 		start:
 			char m;
-cout<<"Веведіть команду: внести запис(w), вивести вміст тексту та найти групу з найбільшою кількустю пропусків(r), очистити файл(k), завершити роботу(q)"<<endl;
+			while(1){
+cout<<"Веведіть команду:"<<endl;
+cout<< "внести запис(w)"<<endl;
+cout<<"вивести вміст тексту та найти групу з найбільшою кількустю пропусків(r)"<<endl;
+cout<<"очистити файл(k)"<<endl;
+cout<<"завершити роботу(q)"<<endl;
 cin>>m;
 switch(m){
 		case 'w':{
-			input (f, elemant);;
+			input (f, IT);;
 			goto start;
 			break;
 		}
 		case 'r':{
-			output(f, elemant);;
+			output(f, IT);;
 			goto start;
 			break;
 		}
@@ -109,11 +115,8 @@ switch(m){
 	
 	default: cout<<"fattal error"<<endl; goto start;
 	}
+	}
 	end:
 		//Read(f, elemant);
-		 
-
-	
-
 	return 0;
 }
